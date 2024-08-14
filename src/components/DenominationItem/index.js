bd36a5a4dbb0e06c0abad2203e3d05e7086b2efb
sadhana631 <indhusweety631@gmail.com>
2024-08-14 15:01:54 +0000

    fourth commit

--- src/components/DenominationItem/index.js
+++ src/components/DenominationItem/index.js
@@ -1,15 +1,12 @@
 import './index.css'
 
-
 const DenominationItem = props => {
   const {denominationDetails, updateBalance} = props
   const {value} = denominationDetails
 
-
   onClickDenomination = () => {
     updateBalance(value)
   }
-
 
   return (
     <li className="denomination-item">
